a72a8a672cc347119e7dc24b1aece0d6951d4100
Ritik Saxena <saxenar444@gmail.com>
2018-04-01 19:05:17 +0530

    add clear text btn

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -18,6 +18,7 @@ export default class Toolbar extends React.Component {
         <Button text="Code block" />
         <Button text="Code" />
         <Button text="Blockquote" />
+        <Button text="Clear Text" />
       </div>
     );
   }
