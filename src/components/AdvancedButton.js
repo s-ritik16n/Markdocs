d6b9ebc57b636fdb620ac30d1c3a8491beec3f9a
Ritik Saxena <saxenar444@gmail.com>
2018-04-01 19:07:21 +0530

    initialize component AdvancedButton

---
+++ src/components/AdvancedButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AdvancedButton extends React.Component {
+
+  render = () => {
+    return (
+      <div>AdvancedButton Component</div>
+    );
+  }
+}
