8e92826b6bda4b2325d97551c519f4a7313b7f67
ritik <saxenar444@gmail.com>
2018-04-17 15:55:37 +0530

    fix export

--- src/utils.js
+++ src/utils.js
@@ -1,53 +1,51 @@
-const copy = (event, text) => {
+export const copy = (event, text) => {
   console.log(text);
 }
 
-const cut = (event, text) => {
+export const cut = (event, text) => {
   console.log(text);
 }
 
-const paste = (event, text) => {
+export const paste = (event, text) => {
   console.log(text);
 }
 
-const headers = (event, text) => {
+export const headers = (event, text) => {
   console.log(text);
 }
 
-const link = (event, text) => {
+export const link = (event, text) => {
   console.log(text);
 }
 
-const image = (event, text) => {
+export const image = (event, text) => {
   console.log(text);
 }
 
-const lists = (event, text) => {
+export const lists = (event, text) => {
   console.log(text);
 }
 
-const table = (event, text) => {
+export const table = (event, text) => {
   console.log(text);
 }
 
-const lineBreak = (event, text) => {
+export const lineBreak = (event, text) => {
   console.log(text);
 }
 
-const codeBlock = (event, text) => {
+export const codeBlock = (event, text) => {
   console.log(text);
 }
 
-const code = (event, text) => {
+export const code = (event, text) => {
   console.log(text);
 }
 
-const blockQuote = (event, text) => {
+export const blockQuote = (event, text) => {
   console.log(text);
 }
 
-const clearText = (event, text) => {
+export const clearText = (event, text) => {
   console.log(text);
 }
-
-export * from  ...;
