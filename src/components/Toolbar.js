eb5baeb04ba631eee0a984d2d79189ed0edc36a2
ritik <saxenar444@gmail.com>
2018-04-17 14:53:41 +0530

    add eventlisteners

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './Button';
+import * as utils from './utils';
 
 export default class Toolbar extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render = () => {
     return (
       <div>
-        <Button text="cut" />
-        <Button text="copy" />
-        <Button text="paste" />
-        <Button text="headers" />
-        <Button text="link" />
-        <Button text="image" />
-        <Button text="lists" />
-        <Button text="table" />
-        <Button text="line break" />
-        <Button text="Code block" />
-        <Button text="Code" />
-        <Button text="Blockquote" />
-        <Button text="Clear Text" />
+        <Button text="cut" onClick={(event) => utils.cut(event, this.props.text)}/>
+        <Button text="copy" onClick={(event) => utils.copy(event, this.props.text)}/>
+        <Button text="paste" onClick={(event) => utils.paste(event, this.props.text)}/>
+        <Button text="headers" onClick={(event) => utils.headers(event, this.props.text)}/>
+        <Button text="link" onClick={(event) => utils.link(event, this.props.text)}/>
+        <Button text="image" onClick={(event) => utils.image(event, this.props.text)}/>
+        <Button text="lists" onClick={(event) => utils.lists(event, this.props.text)}/>
+        <Button text="table" onClick={(event) => utils.table(event, this.props.text)}/>
+        <Button text="line break" onClick={(event) => utils.lineBreak(event, this.props.text)}/>
+        <Button text="Code block" onClick={(event) => utils.codeBlock(event, this.props.text)}/>
+        <Button text="Code" onClick={(event) => utils.code(event, this.props.text)}/>
+        <Button text="Blockquote" onClick={(event) => utils.blockQuote(event, this.props.text)}/>
+        <Button text="Clear Text" onClick={(event) => utils.clearText(event, this.props.text)}/>
       </div>
     );
   }
