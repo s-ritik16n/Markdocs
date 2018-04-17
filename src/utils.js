0e4c8322e85e4a06f07f77ea6df8d3a96a0e22fe
ritik <saxenar444@gmail.com>
2018-04-17 14:53:51 +0530

    add button click utils

---
+++ src/utils.js
@@ -0,0 +1,53 @@
+const copy = (event, text) => {
+  console.log(text);
+}
+
+const cut = (event, text) => {
+  console.log(text);
+}
+
+const paste = (event, text) => {
+  console.log(text);
+}
+
+const headers = (event, text) => {
+  console.log(text);
+}
+
+const link = (event, text) => {
+  console.log(text);
+}
+
+const image = (event, text) => {
+  console.log(text);
+}
+
+const lists = (event, text) => {
+  console.log(text);
+}
+
+const table = (event, text) => {
+  console.log(text);
+}
+
+const lineBreak = (event, text) => {
+  console.log(text);
+}
+
+const codeBlock = (event, text) => {
+  console.log(text);
+}
+
+const code = (event, text) => {
+  console.log(text);
+}
+
+const blockQuote = (event, text) => {
+  console.log(text);
+}
+
+const clearText = (event, text) => {
+  console.log(text);
+}
+
+export * from  ...;
