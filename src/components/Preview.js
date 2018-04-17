01eeb90a954dc62f53cbf11575122232fed53fde
ritik <saxenar444@gmail.com>
2018-04-17 15:55:47 +0530

    fallback condition

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -8,7 +8,7 @@ export default class Preview extends React.Component {
 
   render = () => {
     return (
-      <div>{this.props.value}</div>
+      <div>{this.props.value || ""}</div>
     );
   }
 }
