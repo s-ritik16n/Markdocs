e96981ff29c9a00dcb99fa83c8dcc9d1c74a6034
ritik <saxenar444@gmail.com>
2018-04-17 14:53:30 +0530

    preview util - initial stage

---
+++ src/advanced-utils.js
@@ -0,0 +1,3 @@
+export const preview = (event, text) => {
+  console.log(text);
+}
--- src/components/MarkdocsApp.js
+++ src/components/MarkdocsApp.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 import Toolbar from './Toolbar';
 import Editor from './Editor';
 import Preview from './Preview';
+import { preview } form './advanced-utils';
 
 export default class MarkdocsApp extends React.Component {
+
+  previewHandler(event, text) => {
+    event.preventDefault();
+    console.log("previewHandler");
+
+    let previewEl = event.target;
+    previewEl.toggleClass("show");
+
+    if(previewEl.classlist.contains("show")) {
+      this.refs.editor.classlist.add("hide");
+      preview(event, text);
+    } else {
+      this.refs.editor.classlist.remove("hide");
+    }
+  }
+
   render = () => {
     return (
       <div>
         <Toolbar />
-        <Editor />
-        <Preview value={"sample"}/>
+        <Editor ref="editor"/>
+        <Button text="Preview" onCLick={(e) => this.previewHandler(event, text);}/>
       </div>
     );
   }
