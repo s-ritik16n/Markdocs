50777c7a4d70827f3bc97bd192f234eb9124ecda
ritik <saxenar444@gmail.com>
2018-04-18 16:32:15 +0530

    beautify

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './Button';
 import * as utils from '../utils';
-import { ButtonGroup, ButtonToolbar } from 'react-bootstrap';
+import {ButtonGroup, ButtonToolbar} from 'react-bootstrap';
 
 export default class Toolbar extends React.Component {
   constructor(props) {
@@ -12,22 +12,22 @@ export default class Toolbar extends React.Component {
   render = () => {
     return (
       <ButtonToolbar>
-      <ButtonGroup>
-        <Button text="cut" handleClick={utils.cut} data={this.props.data}/>
-        <Button text="copy" handleClick={utils.copy} data={this.props.data}/>
-        <Button text="paste" handleClick={utils.paste} data={this.props.data}/>
-        <Button text="headers" handleClick={utils.headers} data={this.props.data}/>
-        <Button text="link" handleClick={utils.link} data={this.props.data}/>
-        <Button text="image" handleClick={utils.image} data={this.props.data}/>
-        <Button text="lists" handleClick={utils.lists} data={this.props.data}/>
-        <Button text="table" handleClick={utils.table} data={this.props.data}/>
-        <Button text="line break" handleClick={utils.lineBreak} data={this.props.data}/>
-        <Button text="Code block" handleClick={utils.codeBlock} data={this.props.data}/>
-        <Button text="Code" handleClick={utils.code} data={this.props.data}/>
-        <Button text="Blockquote" handleClick={utils.blockQuote} data={this.props.data}/>
-        <Button text="Clear Text" handleClick={utils.clearText} data={this.props.data}/>
-      </ButtonGroup>
-    </ButtonToolbar>
+        <ButtonGroup>
+          <Button text="cut" handleClick={utils.cut} data={this.props.data}/>
+          <Button text="copy" handleClick={utils.copy} data={this.props.data}/>
+          <Button text="paste" handleClick={utils.paste} data={this.props.data}/>
+          <Button text="headers" handleClick={utils.headers} data={this.props.data}/>
+          <Button text="link" handleClick={utils.link} data={this.props.data}/>
+          <Button text="image" handleClick={utils.image} data={this.props.data}/>
+          <Button text="lists" handleClick={utils.lists} data={this.props.data}/>
+          <Button text="table" handleClick={utils.table} data={this.props.data}/>
+          <Button text="line break" handleClick={utils.lineBreak} data={this.props.data}/>
+          <Button text="Code block" handleClick={utils.codeBlock} data={this.props.data}/>
+          <Button text="Code" handleClick={utils.code} data={this.props.data}/>
+          <Button text="Blockquote" handleClick={utils.blockQuote} data={this.props.data}/>
+          <Button text="Clear Text" handleClick={utils.clearText} data={this.props.data}/>
+        </ButtonGroup>
+      </ButtonToolbar>
     );
   }
 }
