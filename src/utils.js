b9c40159990f4d627ffffcffb3e3c71c5778e780
ritik <saxenar444@gmail.com>
2018-04-30 12:17:12 +0530

    add callbcak to all utils

--- src/utils.js
+++ src/utils.js
@@ -4,62 +4,80 @@ export const bold = (event, text, cb) => {
   cb(text);
 }
 
-export const italic = (event, text) => {
+export const italic = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const underline = (event, text) => {
+export const underline = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const headers = (event, text) => {
+export const headers = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const link = (event, text) => {
+export const link = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const image = (event, text) => {
+export const image = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const ulList = (event, text) => {
+export const ulList = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const olList = (event, text) => {
+export const olList = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const table = (event, text) => {
+export const table = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const rule = (event, text) => {
+export const rule = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const code = (event, text) => {
+export const code = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const codeBlock = (event, text) => {
+export const codeBlock = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const blockQuote = (event, text) => {
+export const blockQuote = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const copy = (event, text) => {
+export const copy = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const download = (event, text) => {
+export const download = (event, text, cb) => {
   console.log(text);
+  cb(text);
 }
 
-export const clearText = (event, text) => {
+export const clearText = (event, text, cb) => {
+  // requires one confirmation
   console.log(text);
+  cb(text);
 }
+
+// headers function will be a dropdown
