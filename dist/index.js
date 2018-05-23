f5286c4a018ef13e818f5e2c72a27d8f792e2db6
ritik <saxenar444@gmail.com>
2018-05-23 14:30:26 +0530

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,10 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var _process = require('process');
+
+var _process2 = _interopRequireDefault(_process);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -28,6 +32,6 @@ app.get("/authcallback", function (req, res) {
   console.log(res);
 });
 
-app.listen(app.get('port'), function (port) {
-  console.log('all eyes at ' + app.get('port'));
+app.listen(_process2.default.env.PORT || 8000, function () {
+  console.log('all eyes at ' + (_process2.default.env.PORT || 8000));
 });
