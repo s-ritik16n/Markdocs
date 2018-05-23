0b92194f8205de3ded754e8a2ecdff9eeb3d0ca3
ritik <saxenar444@gmail.com>
2018-05-23 11:10:44 +0530

    before deploying on heroku

--- server/index.js
+++ server/index.js
@@ -12,4 +12,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get("/authcallback", (req, res) => {
+  console.log(res);
+});
+
 app.listen('9000', () => { console.log("all eyes at 9000"); })
