433e8d3ed864ed8fa84abb8f4c577d0dc0521469
ritik <saxenar444@gmail.com>
2018-05-28 13:42:00 +0530

    cannot remove dist

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var config = exports.config = {
+  'CLIENT_ID': 'b5422eace97554dec4b5',
+  'CLIENT_SECRET': '84637d3ad99289a0b4aeb030a636d4fbfc399793'
+};
