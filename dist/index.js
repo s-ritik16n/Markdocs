dff75a8bbb0761d69588de72e25d5e5a12eaf78f
ritik <saxenar444@gmail.com>
2018-05-28 13:57:49 +0530

    add build

--- dist/index.js
+++ dist/index.js
@@ -45,26 +45,26 @@ app.get("/authcallback", function (req, res) {
   var code = req.query.code;
   var state = req.query.state;
   console.log("here is a log outside axios");
-  (0, _axios2.default)({
-    method: 'post',
-    url: 'https://github.com/login/oauth/access_token',
-    headers: { 'Accept': 'application/json' },
-    data: {
-      code: code,
-      state: state,
-      client_id: _process2.default.env.CLIENT_ID || _config2.default['CLIENT_ID'],
-      client_secret: _process2.default.env.CLIENT_SECRET || _config2.default['CLIENT_SECRET']
-    }
-  }).then(function (response) {
-    console.log("here i get the response - then");
-    console.log(response.data);
-    resolve(response.data);
+  try {
+    (0, _axios2.default)({
+      method: 'post',
+      url: 'https://github.com/login/oauth/access_token',
+      headers: { 'Accept': 'application/json' },
+      data: {
+        code: code,
+        state: state,
+        client_id: _process2.default.env.CLIENT_ID || _config2.default['CLIENT_ID'],
+        client_secret: _process2.default.env.CLIENT_SECRET || _config2.default['CLIENT_SECRET']
+      }
+    }).then(function (response) {
+      console.log("here i get the response - then");
+      console.log(response.data);
+      res.redirect('/');
+    });
+  } catch (e) {
+    console.log('error in axios promise - ' + e);
     res.redirect('/');
-  }).catch(function (err) {
-    console.log("here i get the response - error");
-    reject(err);
-    res.redirect('/');
-  });
+  }
 });
 
 app.listen(_process2.default.env.PORT || 8000, function () {
