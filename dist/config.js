e0eddf6b81d603a1f3d3d3ece2d2da19ef75e11a
ritik <saxenar444@gmail.com>
2018-05-28 14:23:22 +0530

    do not ignore config.js

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var config = exports.config = {
+  'CLIENT_ID': process.env.CLIENT_ID,
+  'CLIENT_SECRET': process.env.CLIENT_SECRET
+};
