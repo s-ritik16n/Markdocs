3c8a5248e16771cf79c1ff982d02722c086226fc
ritik <saxenar444@gmail.com>
2018-06-06 21:26:15 +0530

    add react router

--- client/src/app.js
+++ client/src/app.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MarkdocsApp from './components/MarkdocsApp';
 import OAuth from './components/OAuth';
+import { Router, browserHistory } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<MarkdocsApp />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={MarkdocsApp} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('app'));
